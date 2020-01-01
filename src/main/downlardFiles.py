2377b09df66e4fccf2b586da44d8b2d037265000
yuto Okuda <yuto1001okuda@gmail.com>
2020-01-01 15:24:52 +0900

    ファイルの種類を識別した拡張子つきで自動ダウンロードを実装\npng, jpg, gif, pdfのみ

--- src/main/downlardFiles.py
+++ src/main/downlardFiles.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[31]:
+# In[ ]:
 
 
 import urllib.request as rq
+import magic
+import json
+
+url = input("Enter the URL of image:")
+fileName = "./tmp/tmp"
+
+data = rq.urlopen(url).read()
 
 
-# In[41]:
+# In[ ]:
 
 
-if __name__ == "__main__" :
-    
-    url = input("Enter the URL of image:")
-    fileName = "./tmp/tmp.png"
-    
-    rq.urlretrieve(url, fileName)
+type = magic.from_buffer(data, mime=True)
+
+with open('./mimeDic.json') as f :
+    mimeDic = json.load(f)
+
+extension = mimeDic[type]
+fileName += extension
+fileName
+
+
+# In[ ]:
+
+
+with open(fileName, mode='wb') as file :
+    file.write(data)
 
 
 # In[ ]:
