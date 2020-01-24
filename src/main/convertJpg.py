db8ed9748ca33e52907c6e7b4061532b6d6aaff4
yuto Okuda <yuto1001okuda@gmail.com>
2020-01-24 17:36:39 +0900

    [change]	pdfファイルのみをjpgに変換するように変更した
    ダウンロード時に画像ではないのはpdfのみなので、他の画像ファイルは画像統合時に頑張ってもらう

--- src/main/convertJpg.py
+++ src/main/convertJpg.py
@@ -8,43 +8,40 @@ import json
 import magic
 import glob
 import pdf2image
-import os
 
 
 # In[ ]:
 
 
-filePath = 'tmp/tmp*'
+def convertPdf(fileName) :
+    saveFileName = fileName.rsplit('.', 1)[0] + '.jpg'
+    
+    image = pdf2image.convert_from_path(fileName, dpi=600)
+    image[0].save(saveFileName, 'JPEG')   
+    print(saveFileName)
+
+
+# In[ ]:
+
+
+filePath = './tmp/tmp*'
 fileLists = sorted(glob.glob(filePath))
 
 
 # In[ ]:
 
 
-def convert_pdf_to_jpg(fileName, page) :
-    saveFileName = fileName.rsplit('.')[0] + '.jpg'
-    
-    image = pdf2image.convert_from_path(fileName, dpi=600)
-    image.save(saveFileName, 'JPEG')    
-
-
-# In[ ]:
-
-
 for fileName in fileLists :
+    print(fileName)
     with open(fileName, mode='rb') as f :
         binary = f.read()
     
     mimeType = magic.from_buffer(binary, mime=True)
-    if mimeType == 'image/png' :
-        print('png')
-    elif mimeType == 'image/gif' :
-        print('gif')
-    elif mimeType == 'application/pdf' :
+    
+    if mimeType == 'application/pdf' :
         print('pdf')
-        convert_pdf_to_jpg(fileName, 1)
-    else :
-        print('unknown format!')
+        convertPdf(fileName)
+        print('compleate!')
 
 
 # In[ ]:
